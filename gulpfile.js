098e24fd7c0474e596e5273e038b4d46836a1a89
Stephen Way <way.stephen@gmail.com>
2015-12-28 09:54:10 -0800

    Update gulpfile.js formatting

--- gulpfile.js
+++ gulpfile.js
@@ -9,47 +9,45 @@ var extend = require('postcss-extend');
 var stylelint = require('stylelint');
 var reporter = require('postcss-reporter');
 
-var stylelintConfig = {
-  "extends": "stylelint-config-suitcss",
-  "rules": {
-    "at-rule-empty-line-before": null,
-    "block-opening-brace-newline-after": null,
-    "comment-empty-line-before": null,
-    "number-leading-zero": null,
-    "string-quotes": "single",
-    "selector-list-comma-newline-after": "always-multi-line",
-    "selector-combinator-space-before": null
-  }
-}
- 
-
+// Check code style of stylesheets
 gulp.task('css:lint', function() {
-        return gulp.src('example/index.css')
-        .pipe(postcss([
-                stylelint(stylelintConfig),
-                reporter({
-                        clearMessages: true,
-                }),
-        ]));
+  var stylelintConfig = {
+    "extends": "stylelint-config-suitcss",
+    "rules": {
+      "at-rule-empty-line-before": null,
+      "block-opening-brace-newline-after": null,
+      "comment-empty-line-before": null,
+      "number-leading-zero": null,
+      "string-quotes": "single",
+      "selector-list-comma-newline-after": "always-multi-line",
+      "selector-combinator-space-before": null
+    }
+  }
+  return gulp.src('example/index.css')
+    .pipe(postcss([
+      stylelint(stylelintConfig),
+      reporter({
+        clearMessages: true,
+      }),
+    ]));
 });
 
 // Compile css into /example
 gulp.task('css', ['css:lint'], function() {
-        var processors = [
-                cssimport,
-                simplevars,
-                extend,
-                nested,
-                autoprefixer(['last 2 versions'])
-        ];
-        return gulp.src('example/index.css')
-        .pipe(postcss(processors).on('error', gutil.log))
-        .pipe(gulp.dest('example/build'));
+  var processors = [
+    cssimport,
+    simplevars,
+    extend,
+      nested,
+      autoprefixer(['last 2 versions'])
+  ];
+  return gulp.src('example/index.css')
+    .pipe(postcss(processors).on('error', gutil.log))
+    .pipe(gulp.dest('example/build'));
 });
 
+// Watch css and compile upon file changed
 gulp.task('watch', function() {
-
-        // Watch css and compile upon file changed
-        gulp.watch('test.css', ['css']);
+  gulp.watch('example/index.css', ['css']);
 });
 
